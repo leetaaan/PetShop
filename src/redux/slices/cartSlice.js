c166a92d2df7c559eb4bddf0749139759e39f359
Le Tan <letan085@gmail.com>
2023-08-24 23:18:43 +0700

    fix

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -37,7 +37,7 @@ const cartSlice = createSlice({
             const existingItem=state.cartItem.find(item=>item.id===id)
 
             if(existingItem){
-                state.cartItem=state.cartItem.filter(item=>item.id !=id)
+                state.cartItem=state.cartItem.filter(item=>item.id !==id)
                 state.totalQuantity=state.totalQuantity-existingItem.quantity
             }
             state.totalAmount = state.cartItem.reduce((total, item) => total + Number(item.price) * Number(item.quantity),0);
